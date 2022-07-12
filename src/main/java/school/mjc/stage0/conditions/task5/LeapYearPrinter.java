f0e17eb3e8a8ce8c4c5674797a72e802933ab245
ShulpinP <pavelshulpin6@gmail.com>
2022-07-12 21:04:03 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int check = year % 4;
+        switch (check) {
+            default:
+                System.out.println("not leap");
+                break;
+            case (0):
+                System.out.println("leap");
+                break;
+        }
     }
 }
